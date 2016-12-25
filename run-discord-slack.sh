caaf376913ec9ed096299a245b8882f18bf396c8
root <root@nero.kronmiller.net>
2016-12-24 22:22:02 -0500

    up??

--- run-discord-slack.sh
+++ run-discord-slack.sh
@@ -2,13 +2,8 @@
 source credentials.sh
 docker pull wkronmiller/discord-slack
 docker rm -f discord-slack
-docker run --name discord-slack -d --link hn-redis:redis \
-    -e DISCORD_CHANNEL_ID='135901657513852929' \
+docker run --name discord-slack -d \
     -e DISCORD_TOKEN=$DISCORD_TOKEN \
-    -e DISCORD_BOT_NAME='Rory' \
-    -e SLACK_TOKEN=$SLACK_DISCORD_TOKEN \
-    -e SLACK_HOOK_URL=$SLACK_URL \
-    -e SLACK_CHANNEL_ID='C38LB145A' \
-    -e SLACK_CHANNEL='discord-gennau' \
-    -e SLACK_USER='U2MHZ4796' \
+    -e SLACK_ADMIN_TOKEN=$SLACK_ADMIN_TOKEN \
+    -e SLACK_BOT_TOKEN=$SLACK_DISCORD_TOKEN \
     wkronmiller/discord-slack
